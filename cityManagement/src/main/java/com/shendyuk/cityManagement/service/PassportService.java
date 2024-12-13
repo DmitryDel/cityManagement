0ea331a2870621b0e31eafc72bbf1a9693ba9b8b
dmitrydel <dmitrydel92@gmail.com>
2024-12-13 20:46:38 +0300

    Создал таблицы в файле createTable.sql

--- cityManagement/src/main/java/com/shendyuk/cityManagement/service/PassportService.java
+++ cityManagement/src/main/java/com/shendyuk/cityManagement/service/PassportService.java
@@ -3,7 +3,6 @@ package com.shendyuk.cityManagement.service;
 import com.shendyuk.cityManagement.model.Passport;
 import com.shendyuk.cityManagement.repository.PassportRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -16,17 +15,16 @@ import java.util.Random;
 public class PassportService {
     private final PassportRepository passportRepository;
 
-//    public Passport createNewPassport() {
-//        String series = generatePassportSeries();
-//        LocalDate issueDate = LocalDate.now();
-//        return new Passport(series, issueDate);
-//    }
-//    public Passport() {
-//    }
+    public Passport createNewPassport() {
+        Passport passport = new Passport();
+        passport.setSeries(generatePassportSeries());
+        passport.setIssueData(LocalDate.now());
+        return passport;
+    }
 
     public static String generatePassportSeries() {
         Random random = new Random();
-        return String.format("%04d", random.nextInt(10000));
+        return String.format("%06d", random.nextInt(1000000));
     }
 
     public List<Passport> findAll() {
