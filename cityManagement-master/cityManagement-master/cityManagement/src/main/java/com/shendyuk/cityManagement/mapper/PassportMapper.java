2728e0a043a3237d6542a00e4a047124b083e11e
DmitryDel <dmitrydel92@gmail.com>
2024-12-18 15:12:33 +0500

    implemented interfaces in the service package

--- cityManagement-master/cityManagement-master/cityManagement/src/main/java/com/shendyuk/cityManagement/mapper/PassportMapper.java
+++ cityManagement-master/cityManagement-master/cityManagement/src/main/java/com/shendyuk/cityManagement/mapper/PassportMapper.java
@@ -3,7 +3,6 @@ package com.shendyuk.cityManagement.mapper;
 import com.shendyuk.cityManagement.dto.PassportCreateRequestDTO;
 import com.shendyuk.cityManagement.dto.PassportResponseDTO;
 import com.shendyuk.cityManagement.model.Passport;
-import com.shendyuk.cityManagement.repository.PassportRepository;
 import org.mapstruct.Mapper;
 
 import java.util.List;
